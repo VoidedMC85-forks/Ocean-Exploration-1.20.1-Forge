9cd6871b5fc33604e49b038c49e2444d6d339631
gdliytj <157420580+gdliytj@users.noreply.github.com>
2024-07-24 22:02:58 -0400

    Initial commit

---
+++ src/main/java/net/isaiah/oceanexploration/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.isaiah.oceanexploration.item;
+
+import net.isaiah.oceanexploration.OceanExploration;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+     public static final DeferredRegister<Item> ITEMS =
+             DeferredRegister.create(ForgeRegistries.ITEMS, OceanExploration.MOD_ID);
+// Copy from public to Item properties then paste below
+     public static final RegistryObject<Item> SHARK_TOOTH = ITEMS.register("shark_tooth",
+             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> Thing = ITEMS.register("thing",
+            () -> new Item(new Item.Properties()));
+
+
+     public static void register(IEventBus eventBus) {
+         ITEMS.register(eventBus);
+     }
+}
